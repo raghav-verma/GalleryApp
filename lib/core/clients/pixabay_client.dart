bd1ff0ec256c3935b8cf4a3aedbdc6a30becd191
raghav-verma <raghavverma.dev@gmail.com>
2024-05-12 11:08:20 +0530

    Final build

---
+++ lib/core/clients/pixabay_client.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:gapopa/features/image_gallery/models/index.dart';
+import 'package:http/http.dart' as http;
+
+class PixabayClient {
+  final String apiKey = '43805107-00753c3f9b57e8d7262ab6790';
+
+  Future<List<ImageModel>> fetchImages(String query, int page) async {
+    final response = await http.get(
+      Uri.parse('https://pixabay.com/api/?key=$apiKey&q=$query&image_type=photo'),
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return (data['hits'] as List).map((item) => ImageModel.fromJson(item)).toList();
+    } else {
+      throw Exception('Failed to load images');
+    }
+  }
+}
